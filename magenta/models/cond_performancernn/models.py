5d00ee10bc23b1ffd9b6b83ffe2224b8fcbeab12
Salman Mohammadi <salman.mohammadi@outlook.com>
2019-02-24 14:24:41 +0000

    Removing debug

--- magenta/models/cond_performancernn/models.py
+++ magenta/models/cond_performancernn/models.py
@@ -2,7 +2,7 @@
 
 from tensorflow.python.util import nest as tf_nest
 from tensorflow.contrib.cudnn_rnn.python.layers import cudnn_rnn
-from tensorflow.python import debug as tf_debug
+
 import tensorflow as tf
 import magenta as mg
 import pandas as pd
@@ -45,8 +45,7 @@ class BaseModel:
                                     logdir=logdir,
                                     hooks=hooks,
                                     save_checkpoint_secs=save_checkpoint_secs,
-                                    save_summaries_steps=save_summaries_steps,
-                                    session_wrapper=tf_debug.LocalCLIDebugWrapperSession)
+                                    save_summaries_steps=save_summaries_steps)
           tf.logging.info('Training loop complete.')
 
     def evaluate(self):
